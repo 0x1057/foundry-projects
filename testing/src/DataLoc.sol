e35952e6f5944517fa45701db096b4320c93dec1
nightfallxyz <real.nightfallxyz@gmail.com>
2024-12-08 13:04:59 -0600

    Data Locations

---
+++ testing/src/DataLoc.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract DataLocations {
+    uint256[] public arr;
+    mapping(uint256 => address) map;
+
+    struct MyStruct {
+        uint256 foo;
+    }
+
+    mapping(uint256 => MyStruct) myStructs;
+
+    function f() public {
+        _f(arr, map, myStructs[1]);
+
+        MyStruct storage myStruct = myStructs[1];
+        MyStruct memory myMemStruct = MyStruct(0);
+    }
+
+    function _f(
+        uint256[] storage _arr,
+        mapping(uint256 => address) storage _map,
+        MyStruct storage _myStruct
+    ) internal {
+        // do something with the storage vars
+    }
+
+    
+    }
+}
